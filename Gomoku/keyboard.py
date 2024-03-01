3ff1150efcc606930f816f77985961c8d3420f4c
Flere210-HAO <876723691@qq.com>
2024-03-01 10:01:56 +0800

    修正一下信息

--- Gomoku/keyboard.py
+++ Gomoku/keyboard.py
@@ -9,9 +9,25 @@ pygame.init()
 screen = pygame.display.set_mode((670,670))
 #设置窗口颜色
 screen_color = [255,255,255] #白色
+line_color = [0,0,0] #设置线条颜色，黑色
 while True: #刷新画布（pygame就像一个画布不断刷新
     for event in pygame.event.get():  #获取事件，如果点击窗口右上角的关闭案件即关闭
         if event.type in (pygame.QUIT,pygame.KEYDOWN):
             sys.exit()
-        screen.fill(screen_color)  #清屏，刷成了screen_color 的颜色
-        pygame.display.update()  #更新pygame这个画布
+    screen.fill(screen_color)  #清屏，刷成了screen_color 的颜色
+
+
+    #pygame.draw.line(screen,line_color,[0,0],[670,670],2)
+    for i in range(27,670,44):
+        #先画竖线
+        if i==27 or i==670-27:
+            pygame.draw.line(screen,line_color,[i,27],[i,670-27],4)
+        else:
+            pygame.draw.line(screen,line_color,[i,27],[i,670-27],2)
+        #
+        if i==27 or i==670-27:
+            pygame.draw.line(screen,line_color,[27,i],[670-27,i],4)
+        else:
+            pygame.draw.line(screen,line_color,[27,i],[670-27,i],2)
+
+    pygame.display.update()  #更新pygame这个画布
